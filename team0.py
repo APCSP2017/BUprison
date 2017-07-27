8731943bdee4005d67af6facd6e4c7df61d3a38c
voichemg <voichemg@npenn.org>
2017-07-27 09:01:54 -0400

    added team 0 code

--- team0.py
+++ team0.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'SchnitHead' # Only 10 chars displayed.
+strategy_name = 'Collude until Betrayed'
+strategy_description = 'Lack of programming background'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +17,11 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if 'b' in their_history:
+        return 'b'
+    else:
+        return 'c'
+   
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -26,7 +30,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    #return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
