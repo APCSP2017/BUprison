dd496db2a67ad4592295902d3c2566f93a788e5f
HGP Local Machine <bpetcaugh@gmail.com>
2017-07-27 09:19:03 -0400

    Changed team 2

--- team2.py
+++ team2.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,9 +7,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Stealers' # Only 10 chars displayed.
+strategy_name = 'Play nice, then stop if they stop.'
+strategy_description = 'Collude until betray, then betray.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,8 +26,38 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
+
     
-    return 'c'
+    if 'b' in their_history:
+        return 'b'
+    else:
+        return 'c'
+    '''
+    if len(my_history) == 0:
+       return 'c'
+    else:
+        their_last_choice = their_history[-1].lower()
+        our_last_choice = my_history[-1].lower()
+        # based on their last choice make our choice
+        if our_last_choice == 'c' and their_last_choice == 'c':
+            x = random.randint(1,100)
+            # 99% chance we'll do a 'c'
+            if x <= 99:
+                return 'c'
+            else:
+                return 'b'
+        elif our_last_choice == 'c' and their_last_choice == 'b':
+            return 'b'
+        elif our_last_choice == 'b' and their_last_choice == 'c':
+            x = random.randint(1,3)
+            # 33% chance we'll do a 'c'
+            if x == 1:
+                return 'c'
+            else:
+                return 'b'
+        else: # if we both chose b
+            return 'b'
+        '''
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
